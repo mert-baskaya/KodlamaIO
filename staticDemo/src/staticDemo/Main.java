a674d366a9846c2a3930c841c7dc151b3c0cc609
Mert Başkaya <mertb1998@gmail.com>
2021-07-12 21:17:40 +0300

    static and inner classes

---
+++ staticDemo/src/staticDemo/Main.java
@@ -0,0 +1,11 @@
+package staticDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DatabaseHelper.Connection.createConnection();
+		//best practice'ler ic ice olan classlari onermiyor
+	}
+
+}
---
+++ staticDemo/src/staticDemo/ProductManager.java
@@ -0,0 +1,11 @@
+package staticDemo;
+
+public class ProductManager {
+	public void add(Product product) {
+		if(ProductValidator.isValid(product)) {
+			System.out.println("Urun eklendi");			
+		} else {
+			System.out.println("Urun bilgileri gecersizdir");
+		}
+	}
+}
