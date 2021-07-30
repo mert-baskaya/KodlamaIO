bc31f195eed094730aed1fc5e3793225ae2af097
Mert Başkaya <mertb1998@gmail.com>
2021-07-30 15:36:39 +0300

    katmanlar eklendi
    iş ilanları için api basit bir controller yazıldı

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "JobPosition")
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "salary")
+	private String salary;
+}
